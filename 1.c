136984ca857646f5836510102be3d02b9dbdf7e7
Javed Alam <141043239+javedalam0786@users.noreply.github.com>
2024-02-15 18:58:21 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+                    // Add two number using fumction.
+int main(){
+    int number1, number2, sum;
+
+    printf("emter two integers");
+    scanf("%d%d",&number1, &number2);
+
+    sum = number1 + number2;
+    printf("%d + %d = %d",number1, number2, sum);
+    return 0;
+    }
