2bd2e52bee43bd6d21ae2f15997e79fdf56de02c
Javed Alam <141043239+javedalam0786@users.noreply.github.com>
2024-02-15 19:00:23 +0530

    Add files via upload

---
+++ 8.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+                        // Find Area of Rectangle
+int main()
+{
+    int width=5;
+    int height=10;
+    int area=width*height;
+    printf("Area of the rectangle=%d",area);
+    return 0;
+}
