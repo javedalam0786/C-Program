143ac47003f71211f5d9beb506ef7a5f3aa02988
Javed Alam <141043239+javedalam0786@users.noreply.github.com>
2024-02-15 19:04:08 +0530

    Add files via upload

---
+++ 19.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+                   // Print the numbers from 0 to n, if n is given by user.
+
+int main () {
+    int n;
+    printf("enter number :");
+    scanf("%d" , &n);
+     int i=0;
+     while(i <= n) {
+        printf("%d\n" , i);
+     } 
+     return 0;
+}
