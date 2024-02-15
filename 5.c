aa979024c37a02d431e14498342bf03c1d7f0392
Javed Alam <141043239+javedalam0786@users.noreply.github.com>
2024-02-15 18:59:26 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+                                  //  add two numbwrs
+
+int main() {
+    int a, b;
+    printf("enter a=");
+    scanf("%d",&a);
+
+    printf("enter b=");
+    scanf("%d",&b);
+    
+    int sum = a + b;
+    printf("sum is : %d",a + b);
+    return 0;
+}
